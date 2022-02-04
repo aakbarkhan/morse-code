b3a12482fb566ac4b70b486216f276cb326adc85
aakbarkhan <aakbarkhan@gmail.com>
2022-02-04 13:26:57 +0530

    fix linters

--- first.rb
+++ first.rb
@@ -1,32 +1,27 @@
 def decode_char(code)
-    morse_code = {
-         '.-' => 'A',
-        '-...' => 'B',
-        '---' => 'O',
-        '-..-' => 'X',
-        '..-.' => 'F',
-        '..-' => 'U',
-        '.-..' => 'L',
-        '.-.' => 'R',
-        '..' => 'I',
-        '.' => 'E',
-        '...' => 'S',
-        '--' => 'M',
-        '-.--' => 'Y'}
-    new_char = ''
-    arr = code.split
-    arr.each do |str|
-        if str.to_s == code
-            new_char += morse_code[str.to_s]
-        end
-    end
-    puts(new_char)
+  morse_code = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '---' => 'O',
+    '-..-' => 'X',
+    '..-.' => 'F',
+    '..-' => 'U',
+    '.-..' => 'L',
+    '.-.' => 'R',
+    '..' => 'I',
+    '.' => 'E',
+    '...' => 'S',
+    '--' => 'M',
+    '-.--' => 'Y'
+  }
+  new_char = ''
+  arr = code.split
+  arr.each do |str|
+    new_char += morse_code[str.to_s] if str.to_s == code
+  end
+  puts(new_char)
 end
 
-decode_char(".-")
-decode_char("-...")
+decode_char('.-')
+decode_char('-...')
 decode_char('-- -.--')
-
-
-
-
