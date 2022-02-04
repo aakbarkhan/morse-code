cc5415503b3c4c4f5b0715f2978e1a4dfec02f08
aakbarkhan <aakbarkhan@gmail.com>
2022-02-04 13:31:10 +0530

    Fix linters

--- first.rb
+++ first.rb
@@ -1,27 +1,25 @@
-def decode_char(code)
-  morse_code = {
-    '.-' => 'A',
-    '-...' => 'B',
-    '---' => 'O',
-    '-..-' => 'X',
-    '..-.' => 'F',
-    '..-' => 'U',
-    '.-..' => 'L',
-    '.-.' => 'R',
-    '..' => 'I',
-    '.' => 'E',
-    '...' => 'S',
-    '--' => 'M',
-    '-.--' => 'Y'
-  }
+def decode(code)
+  mor = { '.-' => 'A',
+          '-...' => 'B',
+          '---' => 'O',
+          '-..-' => 'X',
+          '..-.' => 'F',
+          '..-' => 'U',
+          '.-..' => 'L',
+          '.-.' => 'R',
+          '..' => 'I',
+          '.' => 'E',
+          '...' => 'S',
+          '--' => 'M',
+          '-.--' => 'Y' }
   new_char = ''
   arr = code.split
   arr.each do |str|
-    new_char += morse_code[str.to_s] if str.to_s == code
+    new_char += mor[str.to_s] if str.to_s == code
   end
   puts(new_char)
 end
 
-decode_char('.-')
-decode_char('-...')
-decode_char('-- -.--')
+decode('.-')
+decode('-...')
+decode('-- -.--')
