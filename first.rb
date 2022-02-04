a0810a3a8c2a99485e3edc659c5e52dc37a8b01e
aakbarkhan <aakbarkhan@gmail.com>
2022-02-04 14:40:55 +0530

    fix linters

--- first.rb
+++ first.rb
@@ -1,35 +1,37 @@
 def decode_char(char)
-    morse_chars_codes = {
-      '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-      '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-      '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-      '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-      '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-      '--..' => 'Z'
-    }
-    morse_chars_codes.each do |key, value|
-      return value if key == char
-    end
+  morse_chars_codes = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+  morse_chars_codes.each do |key, value|
+    return value if key == char
   end
-  
-  def decode_word(word)
-    str_val = ''
-    word.split.each do |char|
-      str_val += decode_char(char)
-    end
-    str_val
+end
+
+def decode_word(word)
+  str_val = ''
+  word.split.each do |char|
+    str_val += decode_char(char)
   end
-  
-  def decode(msg)
-    msg_split = msg.split('   ')
-    msg_sentence = ''
-    msg_split.each do |word|
-      msg_sentence += "#{decode_word(word)} "
-    end
-    msg_sentence.strip
+  str_val
+end
+
+def decode(msg)
+  msg_split = msg.split('   ')
+  msg_sentence = ''
+  msg_split.each do |word|
+    msg_sentence += "#{decode_word(word)} "
   end
-  
-  puts decode_char('.-')
-  puts decode_word('-- -.--')
-  puts decode('.- -.- ..-   --.. .- -.-. .... . .   .- -. -..   .-. --- .-.. .- -. -..   .... .- -..   .-   --. --- --- -..   -.-. --- -.. .. -. --.   ... . ... ... .. --- -.')
-  puts decode('..   .-.. --- ...- .   -.-. --- -.. .. -. --.')
+  msg_sentence.strip
+end
+
+puts decode_char('.-')
+puts decode_word('-- -.--')
+puts decode('.- -.- ..-   --.. .- -.-. .... . .   .- -. -..
+    .-. --- .-.. .- -. -..   .... .- -..   .-   --. --- --- -..
+    -.-. --- -.. .. -. --.   ... . ... ... .. --- -.')
+puts decode('..   .-.. --- ...- .   -.-. --- -.. .. -. --.')
